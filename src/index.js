395bd65954ba9f7e3152403843aa49af1adf500b
t-sin <shinichi.tanaka45@gmail.com>
2019-02-25 12:12:57 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+export const parse = (str) => {
+};
+
+export const DEFAULT_RENDERER = {
+};
+
+export const render = (sexp, renderer = DEFAULT_RENDERER) => {
+};
---
+++ test/parse.test.js
@@ -0,0 +1,10 @@
+import { parse } from 'minarai-markup-notation';
+
+test('normal case', () => {
+});
+
+test('edge case', () => {
+});
+
+test('errors', () => {
+});
